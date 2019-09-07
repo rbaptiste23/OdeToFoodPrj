a42c44cc7821852f60fb65517bfa58c70a342a6b
Riley Baptiste <C5RB00@justritemfg.com>
2019-09-06 23:12:14 -0500

    MVC Services

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+      
+
+        public InMemoryRestaurantData()
+            {
+            restaurants = new List<Restaurant>()
+                {
+                    new Restaurant { Id = 1, Name="Scott's Pizza", Cuisine  = CusineType.Italian },
+                    new Restaurant { Id = 1, Name="Scott's Pizza", Cuisine  = CusineType.Italian},
+                    new Restaurant { Id = 1, Name="Scott's Pizza", Cuisine  = CusineType.Italian }
+                };
+                
+                    
+            }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+    
+}
